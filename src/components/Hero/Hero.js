45df3da7f352c7801eca6fda17eafd4f557e80a9
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-19 16:36:14 -0500

    Added welcome banner and small summary with backgorund animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello, I'm Rogelio Lozano <br/>
+        Welcome to my Portfolio!
+      </SectionTitle>
+      <SectionText>
+        Started my journey as a developer during 2022 and have been able to create a varitety of projects.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/LozanoRogelio'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
